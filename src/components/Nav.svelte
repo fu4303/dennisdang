70e63166ed296add07cf42bc758abc6415ad9df9
Dennis Dan <dangggdennis@gmail.com>
2019-07-20 10:42:39 -0400

    Migrate nav

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,60 +1,77 @@
 <script>
-	export let segment;
+  export let segment;
 </script>
 
 <style>
-	nav {
-		border-bottom: 1px solid rgba(255,62,0,0.1);
-		font-weight: 300;
-		padding: 0 1em;
-	}
+  nav ul {
+    display: flex;
+    justify-content: flex-end;
+    list-style: none;
+    margin-top: 1rem;
+    margin-right: 1rem;
+  }
 
-	ul {
-		margin: 0;
-		padding: 0;
-	}
+  nav li a {
+    margin-left: 0.5rem;
+    margin-right: 0.5rem;
+  }
 
-	/* clearfix */
-	ul::after {
-		content: '';
-		display: block;
-		clear: both;
-	}
+  nav li a:hover {
+    text-decoration: none;
+  }
 
-	li {
-		display: block;
-		float: left;
-	}
+  nav li a.blog {
+    background: #419edadb;
+    border-radius: 3px;
+    text-align: center;
+    width: 60px;
+    transition: ease-out 0.15s;
+    color: #fff;
+  }
 
-	.selected {
-		position: relative;
-		display: inline-block;
-	}
+  nav li a.blog:hover {
+    background: #419eda;
+  }
 
-	.selected::after {
-		position: absolute;
-		content: '';
-		width: calc(100% - 1em);
-		height: 2px;
-		background-color: rgb(255,62,0);
-		display: block;
-		bottom: -1px;
-	}
-
-	a {
-		text-decoration: none;
-		padding: 1em 0.5em;
-		display: block;
-	}
+  a {
+	display: block;
+	font-size: 18px;
+	text-decoration: none;
+	padding: 4px;
+  }
 </style>
 
 <nav>
-	<ul>
-		<li><a class='{segment === undefined ? "selected" : ""}' href='.'>home</a></li>
-		<li><a class='{segment === "about" ? "selected" : ""}' href='about'>about</a></li>
+  <ul>
+    <li>
+      <a class={segment === undefined ? 'selected' : ''} href=".">home</a>
+    </li>
+    <li>
+      <a
+        class={segment === 'twitter' ? 'selected' : ''}
+        href="https://twitter.com/dangitdennis"
+        target="_blank">
+        twitter
+      </a>
+    </li>
+    <li>
+      <a
+        class={segment === 'github' ? 'selected' : ''}
+        href="https://github.com/dangdennis"
+        target="_blank">
+        github
+      </a>
+    </li>
 
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
+    <!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch class='{segment === "blog" ? "selected" : ""}' href='blog'>blog</a></li>
-	</ul>
+    <li>
+      <a
+        rel="prefetch"
+        class={`${segment === 'blog' ? 'selected' : ''} blog`}
+        href="blog">
+        blog
+      </a>
+    </li>
+  </ul>
 </nav>
